4ba725d95322c53a75cc21f55f25a0bee59a3127
Tom Dyson <tom.dyson@roh.org.uk>
2023-04-04 09:53:00 +0100

    Explain results

--- nldb/api.py
+++ nldb/api.py
@@ -1,18 +1,15 @@
 import os
-from timeit import default_timer as timer
 from typing import Union
 
 import uvicorn
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
-from nldb.core import get_prompt_template, text_to_sql, execute_sql
+from nldb.core import get_prompt_template, text_to_sql, sql_to_answer
 
 UVICORN_HOST = os.environ.get("UVICORN_HOST", "0.0.0.0")
 UVICORN_PORT = int(os.environ.get("UVICORN_PORT", 8080))
 
-
-# FastAPI app
 app = FastAPI()
 
 
@@ -23,26 +20,30 @@ def template_loader():
 
 
 @app.get("/api/ask")
-def ask(q: Union[str, None] = None):
-    start = timer()
-    (sql_statement, tokens) = text_to_sql(prompt_template, q)
-    generation_time = timer() - start
-    if generation_time < 0.0001:
-        tokens = 0  # cached results don't count towards tokens
-    start = timer()
-    result = execute_sql(sql_statement)
-    execution_time = timer() - start
+async def ask(q: Union[str, None] = None):
+    (sql_statement, tokens_1, time_1) = text_to_sql(prompt_template, q)
+    (results, answer, tokens_2, sql_time, time_2) = sql_to_answer(
+        sql_statement, prompt_template, q
+    )
+    tokens = tokens_1 + tokens_2
     return {
         "response": {
-            "answer": result,
+            "answer": answer,
+            "results": results,
             "sql": sql_statement,
-            "duration": [generation_time, execution_time],  # in seconds
+            "timings": [time_1, sql_time, time_2],  # in seconds
             "tokens": tokens,
-            "cost": tokens / 1000 * 0.002,  # gpt-3.5 is $0.002 per 1000 tokens,
+            "cost": (tokens_1 + tokens_2)
+            / 1000
+            * 0.002,  # gpt-3.5 is $0.002 per 1000 tokens,
         }
     }
 
 
+@app.get("/")
+async def read_index():
+    return FileResponse("index.html")
+
+
 def serve():
-    app.mount("/", StaticFiles(directory="./", html=True), name="static")
     uvicorn.run(app, host=UVICORN_HOST, port=UVICORN_PORT)
--- nldb/core.py
+++ nldb/core.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 from functools import lru_cache
+from timeit import default_timer as timer
 from typing import Tuple
 
 from openai import ChatCompletion
@@ -17,23 +18,69 @@ def get_prompt_template() -> str:
 
 
 @lru_cache
-def text_to_sql(prompt_template: str, question: str) -> Tuple[str, int]:
+def text_to_sql(prompt_template: str, question: str) -> Tuple[str, int, float]:
     # uses GPT-3.5 to convert a question into a SQL statement
     prompt_messages = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": prompt_template % question},
     ]
 
+    start = timer()
     resp = ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=prompt_messages,
         temperature=0,
     )
+    gpt_time = timer() - start
 
     sql = resp["choices"][0]["message"]["content"].strip()
     tokens = resp["usage"]["total_tokens"]
 
-    return (sql, tokens)
+    return (sql, tokens, gpt_time)
+
+
+@lru_cache
+def sql_to_answer(
+    sql: str, prompt_template: str, question: str
+) -> Tuple[str, str, int, float, float]:
+    start = timer()
+    (columns, raw_results) = execute_sql(sql)
+    execution_time = timer() - start
+    formatted_results = tabulate(raw_results, headers=list(columns))
+    clean_columns = [column[0].replace("_", " ") for column in columns]
+    html_results = tabulate(raw_results, headers=clean_columns, tablefmt="html")
+
+    sql_results_message = (
+        "I executed the SQL statement and got the following results:\n\n"
+        + formatted_results
+    )
+
+    prompt_messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt_template % question},
+        {"role": "assistant", "content": sql},
+        {"role": "system", "content": sql_results_message},
+        {
+            "role": "system",
+            "content": f"Using these results, answer the question: {question}",
+        },
+        {
+            "role": "system",
+            "content": "Don't say what the query was, just answer the question.",
+        },
+    ]
+
+    gpt_start = timer()
+    resp = ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=prompt_messages,
+        temperature=0,
+    )
+    gpt_time = timer() - gpt_start
+
+    answer = resp["choices"][0]["message"]["content"].strip()
+    tokens = resp["usage"]["total_tokens"]
+    return (html_results, answer, tokens, execution_time, gpt_time)
 
 
 def execute_sql(query):
@@ -42,7 +89,5 @@ def execute_sql(query):
     cursor = connection.cursor()
     cursor.execute(query)
     result = cursor.fetchall()
-    # make the column names more readable
-    columns = [description[0].replace("_", " ") for description in cursor.description]
-    return tabulate(result, headers=columns, tablefmt="html")
-    # return tabulate(result, headers=columns)
+    columns = list(cursor.description)
+    return (columns, result)
