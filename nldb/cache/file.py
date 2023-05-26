6f27862cdcf5555cfad91b39818221ed02fc0f19
zerolab <dan@zerolab.org>
2023-05-26 15:37:55 +0100

    Hash the key

--- nldb/cache/file.py
+++ nldb/cache/file.py
@@ -1,3 +1,4 @@
+import hashlib
 import json
 import time
 from pathlib import Path
@@ -8,16 +9,23 @@ from .base import CacheBackend
 
 class FileCache(CacheBackend):
     def __init__(
-        self, cache_dir: Optional[str] = "cache", ttl: Optional[int] = None
+        self, cache_dir: Optional[str] = None, ttl: Optional[int] = None
     ) -> None:
-        self.cache_dir = Path(cache_dir)
+        self.cache_dir = Path(cache_dir or "cache")
         self.ttl = ttl
 
         # ensure the cache directory exists
         self.cache_dir.mkdir(parents=True, exist_ok=True)
 
+    def _hash_key(self, key: str) -> str:
+        """
+        Hashes the given key.
+        """
+        return hashlib.sha256(str(key).encode("utf-8")).hexdigest()
+
     def _get_cache_path(self, key: str) -> Path:
-        return self.cache_dir / f"{key}.json"
+        hashed_key = self._hash_key(key)
+        return self.cache_dir / f"{hashed_key}.json"
 
     def _is_cache_expired(self, cache_path: Path) -> bool:
         """
@@ -37,7 +45,12 @@ class FileCache(CacheBackend):
         """
         cache_path = self._get_cache_path(key)
         if cache_path.is_file() and not self._is_cache_expired(cache_path):
-            return json.loads(cache_path.read_text())
+            try:
+                return json.loads(cache_path.read_text())
+            except json.JSONDecodeError:
+                # if the cache file is corrupted, delete it
+                cache_path.unlink()
+                return None
         return None
 
     def set(self, key: str, value: bytes, ttl: Optional[int] = None) -> None:
